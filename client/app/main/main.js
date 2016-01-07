859286b51971fbeb5e7c11f22ec35e7ea6243525
bbalaran <balaran202@gmail.com>
2016-01-06 18:32:54 -0800

    points rendering on main.html page

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -1,5 +1,31 @@
 angular.module('greenfield.main', [])
   .controller('mainController', function($scope, main) {
+
+    //instantiate new map
+    L.mapbox.accessToken = 'pk.eyJ1IjoiYmJhbGFyYW4iLCJhIjoiUmt5TlVjayJ9.1AYg44v3_Bg1XUQ6-5dGAw';
+
+    var map = L.mapbox.map('map', 'mapbox.streets', {
+      scrollWheelZoom: false
+    }).setView([38.8929, -77.0252], 14);
+
+    //render point
+    var geojson = [{
+      "type": "FeatureCollection",
+      "features": [{
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-77.018387, 38.974193]
+        },
+        "properties": {
+          "title": "Takoma Station Tavern"
+        }
+      }]
+    }]
+
+    var myLayer = L.mapbox.featureLayer().addTo(map);
+    myLayer.setGeoJSON(geojson);
+
     $scope.message = '';
 
     main.get().then(function(data) {
