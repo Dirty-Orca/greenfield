f99f39cd4faf4e61bd4666c0a874fccbcd1da12b
Brian Sweeney <bsweeney930@gmail.com>
2016-01-07 11:01:42 -0800

    formatting dates as they are input

--- client/app/search/search.js
+++ client/app/search/search.js
@@ -4,15 +4,21 @@ angular.module('greenfield.search', [])
     
     $scope.search = {};//defining the object and properties ahead of time ensures they are in the correct order for the API request
 
-    $scope.search.loc = '';
+    $scope.search.city = '';
+
+    $scope.state = ''
 
     $scope.search.fromDate = '';
 
     $scope.search.toDate = '';
 
-    $scope.someFunction = function(stuff){
+    $scope.format = function(stuff){
       $log.info(stuff)
+      stuff.formattedToDate = "" + stuff.toDate.getFullYear() + "-" + (stuff.toDate.getMonth() + 1) + "-" + stuff.toDate.getDate()
+      stuff.formattedFromDate = "" + stuff.fromDate.getFullYear() + "-" + (stuff.fromDate.getMonth() + 1) + "-" + stuff.fromDate.getDate()
     }
+
+
 
 
   })
