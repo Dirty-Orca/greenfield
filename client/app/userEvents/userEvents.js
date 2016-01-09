07909ed1e264a0b673271733f1c5c95714fe2740
Brian Sweeney <bsweeney930@gmail.com>
2016-01-09 12:33:39 -0800

    nav bar on search and events page

--- client/app/userEvents/userEvents.js
+++ client/app/userEvents/userEvents.js
@@ -1,7 +1,6 @@
 angular.module('greenfield.userEvents',[])
 
   .controller('userEventsController', function($scope, $log){
-    $log.info('butts');
 
     // $scope.go = function(event){        //in case we don't like the ng-href method, we can change it to an ng-click
     //   location.assign("event.url");    //will do a reroute to http://127.0.0.1:3000/#/www.google.com instead of a redirect if the http is missing
