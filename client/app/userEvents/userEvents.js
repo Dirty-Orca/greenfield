54ba5146b3d8dbeafb9b66643009aa7346e97a3e
Brian Sweeney <bsweeney930@gmail.com>
2016-01-09 16:07:58 -0800

    reimplemented changes from the now dead events_page branch, and added a nav bar to all the pages. reformatted css page.

--- client/app/userEvents/userEvents.js
+++ client/app/userEvents/userEvents.js
@@ -1,5 +1,36 @@
 angular.module('greenfield.userEvents',[])
 
   .controller('userEventsController', function($scope, $log){
-    $log.info('butts');
+
+    // $scope.go = function(event){        //in case we don't like the ng-href method, we can change it to an ng-click
+    //   location.assign("event.url");    //will do a reroute to http://127.0.0.1:3000/#/www.google.com instead of a redirect if the http is missing
+    // }
+
+    $scope.user = {};
+
+    $scope.user.events = [
+      {
+        name: "The Crazy Donkey",//double check that the name field will be the venue name
+        date_time: new Date(),
+        ticket_url: "https://www.ticketfly.com",
+        id: 1,
+        Artist: "William Mayes"
+      },
+      {
+        name: "The Cray Donkey",
+        date_time: new Date(),
+        ticket_url: "https://www.ticketfly.com",
+        id: 1,
+        Artist: "William Mayes"
+      },
+      {
+        name: "The Crazy Donk",
+        date_time: new Date(),
+        ticket_url: "https://www.ticketfly.com",
+        id: 1,
+        Artist: "William Mayes"
+      }
+    ]
+
+
   });
